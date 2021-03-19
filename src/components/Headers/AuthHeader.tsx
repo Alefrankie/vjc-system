0a6eb2fb2d92616093854fd64685a500dcfe90d4
Alefrankie <Alefrank.m@gmail.com>
2021-03-19 13:28:33 -0400

    commit

---
+++ src/components/Headers/AuthHeader.tsx
@@ -0,0 +1,11 @@
+import AuthNavbar from 'components/Navbars/AuthNavbar'
+
+function AuthHeader (): React.ReactElement {
+  return (
+    <main>
+      <AuthNavbar />
+    </main>
+  )
+}
+
+export default AuthHeader
