3428623b34041cd6c4f0ada5689286827778de2b
Alefrankie <alefrank.m@gmail.com>
2022-12-19 11:46:32 -0400

    refactor: not summary

--- src/routes/(app)/api/orders/revenues/+server.ts
+++ src/routes/(app)/api/orders/revenues/+server.ts
@@ -1,11 +1,12 @@
 import { Order } from '$lib/database/schemas/Order'
+import { OrderTypeEnum } from '$lib/enums/OrderTypeEnum'
 import type { RequestHandler } from '@sveltejs/kit'
 
 export const POST: RequestHandler = async ({ request }) => {
 	const body = await request.json()
 	const { query, limit, page } = body
 
-	const data = await Order.find({ ...query, type: { $not: { $regex: 'Budget' } } })
+	const data = await Order.find({ ...query, type: { $not: OrderTypeEnum.BUDGET } })
 		.limit(limit)
 		.skip(limit * page - limit)
 		.sort({ code: -1 })
